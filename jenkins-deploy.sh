8d421559c998b7d0c1a795fc03cda9c65d9df6fa
haoyong <yong.hao@turingsenseai.com>
2021-03-20 16:03:51 +0800

    ✨feat: jenkins-deploy.sh

---
+++ jenkins-deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# NODE_HOME=/opt/nodejs/v8.11.4
+# export PATH=$NODE_HOME/bin:$PATH
+# export NODE_PATH=$NODE_HOME/lib/node_modules
+echo '=====shell start===='
+node -v
+npm -v
+ls -al
+npm i
+pwd
+npm run build
